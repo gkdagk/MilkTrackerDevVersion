be6d4be9261a614244f3128a98a0503c34df9659
Samuel Charles <itsABIApps@gmail.com>
2016-01-13 04:43:12 +0530

    building login in UI

--- app/app.js
+++ app/app.js
@@ -3,8 +3,9 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
+  'myApp.landing',
   'myApp.dashboard'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/dashboard'});
+  $routeProvider.otherwise({redirectTo: '/landing'});
 }]);
